71ca4a9ae4cb2bf03561e77fb944089bfac58f41
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-12-07 17:20:08 +0100

    first commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { response } from 'express';
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: '30mb'}));
+app.use(cors());
+
+app.get(request, response) {
+    response.send("ket noi thanh cong");
+}
 
 const PORT = 5000;
 app.listen(PORT, () => {console.log(`Server is running in the port ${PORT}`)})
