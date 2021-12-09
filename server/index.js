cbd84790781fd1ef9bf0e1f3e3a25e2249fb2403
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-12-09 12:22:24 +0100

    successfully connected to DB

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
+// const express = require('express');
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import router from './routers/posts.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true, limit: '30mb'}));
+app.use(bodyParser.json({limit: '30mb'}));
+app.use(bodyParser.urlencoded({extended: true, limit: '30mb'}));
 app.use(cors());
 
-app.use("/posts", router)
-
 const PORT = 5000;
-app.listen(PORT, () => {console.log(`Server is running in the port ${PORT}`)})
+const URI = "mongodb+srv://thanhvo:node123@cluster0.9ivdt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+app.use("/post", router);
+
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log('Connected to DB');
+    app.listen(PORT, () => {
+        console.log(`Server is running at the port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log('err', err);
+});
+
+// app.get("/", (request, response) => {
+//     response.send("a réussi");
+// })
--- server/routers/posts.js
+++ server/routers/posts.js
@@ -4,7 +4,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get("/", (request, response) => {
-    response.send("un message de thanhcongvo");
+    response.send("un message privé par thanhcongvo");
 })
 
 export default router;
